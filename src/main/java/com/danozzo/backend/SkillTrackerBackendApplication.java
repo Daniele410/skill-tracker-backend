ce59c8e8386d01a8fab54fbc3f36be017a33cc09
Daniele410 <miraglia.daniele410@gmail.com>
2025-04-05 10:55:50 +0200

    first commit, config database mysql

---
+++ src/main/java/com/danozzo/backend/SkillTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.danozzo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillTrackerBackendApplication.class, args);
+    }
+
+}
